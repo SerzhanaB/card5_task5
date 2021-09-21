ace9265c8e16c7922ec4d89bbadbbc246387db89
Serzhana <buraevase@gmail.com>
2021-09-21 18:56:33 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+super(props)
+this.state={ reverted: false}
+  }
+  
+componentWillMount() {
+  console.log('монтирование');
+
+}
+
+componentDidMount() {
+  console.log('did mount');
+
+}
+componentWillReceiveProps(nextProps) {
+
+  console.log('componentWillReceiveProps - not recommended');
+}  
+
+UNSAFE_componentWillUpdate (){
+  console.log('componentWillUpdate - not recommended');
+}
+
+componentWillUnmount() {
+
+  console.log('WillUnmount');
+}
+  revert =()=> this.setState({reverted:!this.state.reverted})
+  render(){
+    return (
+      <div className="App">
+   <h3>Методы жизненного цикла</h3>
+   <button onClick ={this.revert}> Revert</button>
+   {this.state.reverted===true ?  <p>Если положительное значение</p>: <p>если отрицательное значение</p> }
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
